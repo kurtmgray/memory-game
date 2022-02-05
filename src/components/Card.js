dc055c179c3b7bd0f32a1189c544c3627c0594fd
scvkurt03 <kurtgray@gmail.com>
2022-02-04 17:00:20 -0800

    new state

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 export default function Card(props) {
+    
+    
     return (
         <div>
             <img 
                 src={props.img} 
                 alt=''
                 id={props.id}
-                onClick={props.handleClick}>    
+                onClick={props.onClick}>    
             </img>
             <h3>{props.name}</h3>
         </div>
--- src/components/Table.js
+++ src/components/Table.js
@@ -4,84 +4,77 @@ import Header from './Header'
 import uniqid from 'uniqid'
 
 export default function Table() {
+    const characters = [
+        {
+         name: 'Bob',
+         img: 'https://picsum.photos/id/1062/200/300',
+         id: uniqid()
+        }, 
+        {
+         name: 'John',
+         img: 'https://picsum.photos/id/1066/200/300',
+         id: uniqid()
+        }, 
+        {
+         name: 'Nancy',
+         img: 'https://picsum.photos/id/1027/200/300',
+         id: uniqid()
+        }, 
+        {
+         name: 'Brian',
+         img: 'https://picsum.photos/id/1074/200/300',
+         id: uniqid()
+        }, 
+        {
+         name: 'Daniel',
+         img: 'https://picsum.photos/id/1084/200/300',
+         id: uniqid()
+        }, 
+        {
+         name: 'Alann',
+         img: 'https://picsum.photos/id/22/200/300',
+         id: uniqid()
+        }, 
+        {
+         name: 'Benny',
+         img: 'https://picsum.photos/id/237/200/300',
+         id: uniqid()
+        }, 
+        {
+         name: 'Linda',
+         img: 'https://picsum.photos/id/548/200/300',
+         id: uniqid()
+        }, {
+         name: 'Glenn',
+         img: 'https://picsum.photos/id/27/200/300',
+         id: uniqid()
+        }, 
+        {
+         name: 'Moana',
+         img: 'https://picsum.photos/id/765/200/300',
+         id: uniqid()
+        }, 
+        {
+         name: 'Tim',
+         img: 'https://picsum.photos/id/144/200/300',
+         id: uniqid()
+        }, 
+        {
+         name: 'Chris',
+         img: 'https://picsum.photos/id/132/200/300',
+         id: uniqid()
+        }, 
+     ]
     const [score, setScore] = useState(0)
     const [hiScore, setHiScore] = useState(0)
-    const [characters, setCharacters] = useState([
-       {
-        name: 'Bob',
-        img: 'https://picsum.photos/id/1062/200/300',
-        isClicked: false,
-        id: uniqid()
-       }, 
-       {
-        name: 'John',
-        img: 'https://picsum.photos/id/1066/200/300',
-        isClicked: false,
-        id: uniqid()
-       }, 
-       {
-        name: 'Nancy',
-        img: 'https://picsum.photos/id/1027/200/300',
-        isClicked: false,
-        id: uniqid()
-       }, 
-       {
-        name: 'Brian',
-        img: 'https://picsum.photos/id/1074/200/300',
-        isClicked: false,
-        id: uniqid()
-       }, 
-       {
-        name: 'Daniel',
-        img: 'https://picsum.photos/id/1084/200/300',
-        isClicked: false,
-        id: uniqid()
-       }, 
-       {
-        name: 'Alann',
-        img: 'https://picsum.photos/id/22/200/300',
-        isClicked: false,
-        id: uniqid()
-       }, 
-       {
-        name: 'Benny',
-        img: 'https://picsum.photos/id/237/200/300',
-        isClicked: false,
-        id: uniqid()
-       }, 
-       {
-        name: 'Linda',
-        img: 'https://picsum.photos/id/548/200/300',
-        isClicked: false,
-        id: uniqid()
-       }, {
-        name: 'Glenn',
-        img: 'https://picsum.photos/id/27/200/300',
-        isClicked: false,
-        id: uniqid()
-       }, 
-       {
-        name: 'Moana',
-        img: 'https://picsum.photos/id/765/200/300',
-        isClicked: false,
-        id: uniqid()
-       }, 
-       {
-        name: 'Tim',
-        img: 'https://picsum.photos/id/144/200/300',
-        isClicked: false,
-        id: uniqid()
-       }, 
-       {
-        name: 'Chris',
-        img: 'https://picsum.photos/id/132/200/300',
-        isClicked: false,
-        id: uniqid()
-       }, 
-    ])
+    const [clicked, setClicked] = useState([])
+    const [characterOrder, setOrder] = useState(
+        characters.map(c => c.id))
     
-    const shuffleCharacters = () => {
-        setCharacters(prevState => {
+
+    
+    const shuffleOrder = () => {
+        setOrder(prevState => {
             let currentIndex = prevState.length; 
             let randomIndex, temp
             console.log(prevState)
@@ -91,65 +84,56 @@ export default function Table() {
                 prevState[currentIndex] = prevState[randomIndex]
                 prevState[randomIndex] = temp
             }
-            return prevState;
+            console.log(prevState)
+            return prevState; 
         })
+        console.log(characterOrder)
     }
 
-    const checkWin = () => {
-        let win = characters.every(c => c.isClicked === true)
-        console.log(win)
-    }
+    const checkGameOver = (event) => {
+        const win = clicked.length === 12
+        const lose = clicked.includes([event.target.id])
+        console.log('win:', win, clicked.length)
+        console.log('lose:', lose, clicked)
 
-    const checkLose = (event) => {
-        // if clicked character isClicked, display game over, then options to shuffle/reset scores/restart
     }
 
     const setScores = () => {
-        setScore(() => {
-            const newScore = characters.filter(character => 
-                character.isClicked ? true : false
-            ).length
-            return newScore
-        })
-        setHiScore(prevState => 
-            // not the correct condition (should be ===), but need to figure out async click/update
-            prevState <= score ? prevState = score : prevState
-        )    
+        const oldHigh = hiScore
+        setScore(prevScore => prevScore + 1)
+        setHiScore(prevHi => {
+            if (oldHigh === score) {
+                return prevHi + 1
+            }
+        })    
     }
 
-    const markClicked = (event) => {
-        setCharacters(prevState => {
-            const newCharItem = prevState.map(item => {
-                if(item.id === event.target.id) {
-                    return {...item, isClicked: true}
-                }
-                return {...item}
-            })
-            return newCharItem
+    // this needs to go in card to keep event listener references in the same component as the listened-to HTML
+    const addCardToClicked = (event) => {
+        setClicked(prevState => {
+            prevState.push(event.target.id)
+            return prevState
         })
     }
 
     // is this ok?
     const handleClick = (event) => {
-        markClicked(event)
-        checkLose()
-        shuffleCharacters()
+        addCardToClicked(event)
+        checkGameOver(event)
+        // if(checkGameOver()){
+        //     return
+        // }
         setScores()
-        checkWin()
+        shuffleOrder()
     }
 
-    const characterCards = characters.map((character) => {
-        return (
-            <Card 
-                key={character.id}    
-                id={character.id}
-                name={character.name}
-                img={character.img}
-                isClicked={character.isClicked}
-                handleClick={handleClick}
-            /> 
-        )
-    })
+    const characterCards = characterOrder.map((id) =>
+        characters.find(character => character.id === id) 
+    )       
+    console.log(characterOrder)
+    console.log(characterCards)
+    
+
     // want header to be sibling of Table... in here now for working purposes.
     // do I need to move all of state up to App (parent)?
     return (
@@ -160,10 +144,31 @@ export default function Table() {
             />
 
             <div>{JSON.stringify(characters)}</div>
-
-
-            {characterCards}
+            {characterCards.map(character => {
+                return (
+                    <Card 
+                        key={character.id}    
+                        id={character.id}
+                        name={character.name}
+                        img={character.img}
+                        onClick={handleClick}
+                    />
+                )     
+            })}
             
         </div>
     )
 }
+
+
+// flow:
+//   new game:
+//      shuffle card order
+//      reset score
+//   on card click
+//      check if the card has been clicked (in clicked array)
+//      yes?
+//          game over
+//      no?
+//          increment score, high score if needed
+//          add card id into clicked array
